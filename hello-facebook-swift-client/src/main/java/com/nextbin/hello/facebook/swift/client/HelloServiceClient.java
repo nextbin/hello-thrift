56d097065302127b7baea7bc2f78ab2270ccd502
zebin <565083491@qq.com>
2018-10-20 19:36:26 +0800

    client

---
+++ hello-facebook-swift-client/src/main/java/com/nextbin/hello/facebook/swift/client/HelloServiceClient.java
@@ -0,0 +1,34 @@
+package com.nextbin.hello.facebook.swift.client;
+
+import com.facebook.nifty.client.FramedClientConnector;
+import com.facebook.swift.service.ThriftClientManager;
+import com.nextbin.hello.facebook.swift.service.HelloService;
+
+import java.util.concurrent.ExecutionException;
+
+import static com.google.common.net.HostAndPort.fromParts;
+
+/**
+ * @author zebin
+ * @since 2018-10-02.
+ */
+public class HelloServiceClient {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        ThriftClientManager clientManager = new ThriftClientManager();
+        HelloService helloService = clientManager.createClient(
+                new FramedClientConnector(fromParts("localhost", 12345)),
+                HelloService.class).get();
+        System.out.println(helloService.hello());
+
+        int max = 100000;
+        Long start = System.currentTimeMillis();
+        for (int i = 0; i < max; i++) {
+            helloService.hello();
+        }
+        Long end = System.currentTimeMillis();
+        Long elapse = end - start;
+        int perform = Double.valueOf(max / (elapse / 1000d)).intValue();
+        System.out.print("thrift " + max + " 次RPC调用，耗时：" + elapse + "毫秒，平均" + perform + "次/秒");
+
+    }
+}
